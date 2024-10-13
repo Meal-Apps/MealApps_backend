fecf1581c51b689bb157e71facfe1876daad2be0
refathossen <refathossen2001@gmail.com>
2024-10-13 20:30:42 +0600

    create manager middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [ManagerController::class, 'store']);
 Route::post('/login', [ManagerController::class, 'login']);
-Route::post('/createuser',[UserController::class, 'createUser'])->middleware('auth:sanctum');
-Route::post('/addbalance/{userID}',[BalanceController::class, 'store'])->middleware('auth:sanctum');
-Route::post('/expense',[ExpenseController::class, 'store'])->middleware('auth:sanctum');
+
 Route::post('/userlogin',[UserController::class, 'loginUser']);
+
+
+Route::middleware(['auth:sanctum', 'manager'])->group(function () {
+    Route::post('/createuser', [UserController::class, 'createUser']);
+    Route::post('/addbalance/{userID}', [BalanceController::class, 'store']);
+    Route::post('/expense', [ExpenseController::class, 'store']);
+});
